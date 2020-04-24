2701098b0601cfd4a2888a635d91878517bb9651
s.sriramprasath@gmail.com <s.sriramprasath@gmail.com>
2020-04-24 16:13:10 +0530

    vechain_package

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const { Framework } = require('@vechain/connex-framework')
-const { Driver, SimpleNet, SimpleWallet, options } =require('@vechain/connex.driver-nodejs')
+const { Driver, SimpleNet, SimpleWallet, options } = require('@vechain/connex.driver-nodejs')
 const wallet = new SimpleWallet()
 var connex;
 connexfun();
-async function connexfun() {
-    const value = await Driver.connect(new SimpleNet('https://sync-testnet.vechain.org'),wallet);
-    console.log(value);
+function connexfun() {
+    return new Promise(async(reslove, reject) => {
+        const value = await Driver.connect(new SimpleNet('https://sync-testnet.vechain.org'), wallet);
+        console.log(value);
 
-    connex = new Framework(value);
-    console.log(connex)
+        connex = new Framework(value);
+        console.log(connex)
+        reslove(connex);
+    })
 }
 // connexfun();
 module.exports.connexfun = connexfun;
